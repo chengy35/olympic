cf63c5c26adc9bcef131a089e7c3606a6becf77e
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-06-01 22:22:32 +0800

    first commit

---
+++ 1-fv/getGMMAndBOW.m
@@ -0,0 +1,60 @@
+function [gmm] = getGMMAndBOW(fullvideoname,vocabDir,descriptor_path,video_dir)
+    pcaFactor = 0.5;
+    totalnumber = 256000;
+    gmmsize = 256;
+    sampleFeatFile = fullfile(vocabDir,'featfile.mat');
+    modelFilePath = fullfile(vocabDir,'gmmvocmodel.mat');
+    if exist(modelFilePath,'file')
+        load(modelFilePath);
+        return;
+    end
+    start_index = 1;
+    end_index = 1;
+    if ~exist(sampleFeatFile,'file') 
+        allAll = zeros(totalnumber,396);
+        num_videos = size(fullvideoname,1);
+        num_samples_per_vid = round(totalnumber / num_videos);
+        warning('getGMMAndBOW : update num_videos only to include training videos')
+        for i = 1:num_videos       
+            timest = tic();        
+            [~,partfile,~] = fileparts(fullvideoname{i});
+            descriptorFile = fullfile(descriptor_path,sprintf('%s.mat',partfile));      
+                if exist(descriptorFile,'file')
+                    load(descriptorFile);
+                else
+                    fprintf('%s not exist !!!',descriptorFile);
+                    [obj,trj,hog,hof,mbhx,mbhy] = extract_improvedfeatures(fullvideoname{i}) ;
+                    save(descriptorFile,'obj','trj','hog','hof','mbhx','mbhy'); 
+                end
+				hog = sqrt(hog);
+				hof = sqrt(hof);
+                mbhx = sqrt(mbhx);mbhy = sqrt(mbhy);
+				all = [hog hof mbhx mbhy];
+                rnsam = randperm(size(all,1));
+                if numel(rnsam) > num_samples_per_vid
+                    rnsam = rnsam(1:num_samples_per_vid);
+                end
+                end_index = start_index + numel(rnsam) - 1;
+                allAll(start_index:end_index,:) = [all(rnsam,:)];
+                start_index = start_index + numel(rnsam);        
+                timest = toc(timest);
+                fprintf('%d/%d -> %s --> %1.2f sec\n',i,num_videos,fullvideoname{(i)},timest);  
+        end
+
+        if end_index ~= totalnumber
+            allAll(end_index+1:totalnumber,:) = [];
+        end
+            fprintf('start computing pca\n');
+            gmm.pcamap.all = princomp(allAll);
+            fprintf('start saving descriptors\n');
+            save(sampleFeatFile,'allAll','gmm','-v7.3');
+    else
+        load(sampleFeatFile);
+    end
+
+    fprintf('start create gmm \n');
+    allProjected = allAll * gmm.pcamap.all(:,1:size(gmm.pcamap.all,1)*pcaFactor);
+    [gmm.means.all, gmm.covariances.all, gmm.priors.all] = vl_gmm(allProjected', gmmsize);
+    fprintf('start saving gmm\n');
+    save(modelFilePath,'gmm');     
+end
---
+++ main.m
@@ -0,0 +1,47 @@
+clear;
+clc;
+% TODO Add paths
+addpath('~/lib/vlfeat/toolbox');
+vl_setup();
+setenv('LD_LIBRARY_PATH','/usr/local/lib/'); 
+addpath('~/lib/liblinear/matlab');
+addpath('~/lib/libsvm/matlab');
+addpath('~/lib/natsort');
+addpath('~/lib/chol_incomplete/');
+addpath('/usr/local/MATLAB/extern/lib/essential/');
+addpath('~/lib/exact_alm_rpca/');
+
+[video_data_dir,video_dir,fullvideoname, videoname,vocabDir,featDir_FV,featDir_LLC,descriptor_path,class_category,actionName] = getconfig();
+	
+	encode = 'fv';
+	fprintf('begin fv encoding\n');
+	st = 1;
+	send = length(videoname);
+	fprintf('Start : %d \n',st);
+	fprintf('End : %d \n',send);
+	addpath('0-trajectory');
+	fprintf('select salient trajectory\n');
+	getIDT(st,send,fullvideoname,descriptor_path);
+	addpath('1-fv');
+	fprintf('getGMM \n');
+	% create GMM model, Look at this function see if parameters are okay for you.
+	[gmm] = getGMMAndBOW(fullvideoname,vocabDir,descriptor_path,video_dir);
+	fprintf('generate Fisher Vectors \n');
+	FVEncodeFeatures_w(fullvideoname,gmm,vocabDir,st,send,featDir_FV,descriptor_path);
+	getVideoDarwin(fullvideoname,featType,featDir_FV,descriptor_path);
+
+	encode = 'llc';
+	fprintf('begin llc encoding\n');
+	addpath('1-cluster');
+	totalnumber = 256000;
+	kmeans_size = 8000;
+	fprintf('clustering \n');
+	centers = SelectSalient(kmeans_size,totalnumber,fullvideoname,descriptor_path,vocabDir);
+	fprintf('llc Encoding now \n');
+	llcEncodeFeatures(centers,fullvideoname,descriptor_path,featDir_LLC,class_category,vocabDir);
+	clear centers;
+
+addpath('2-trainAndtest');
+%trainAndTest_normalizedL2_LLC(video_dir,fullvideoname,featDir_LLC,encode,actionName);
+trainAndTest_normalizedL2_FV(video_dir,fullvideoname,featDir_FV,encode,actionName);
+%trainAndTest_normalizedL2_FV_LLC(video_data_dir,fullvideoname,featDir_FV,featDir_LLC,encode,actionName);
